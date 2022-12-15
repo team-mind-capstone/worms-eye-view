9d08a7d5987e692b651f3672d43f6fbd480af5bf
Kevin Lam <mtn013209@gmail.com>
2022-12-15 14:43:26 -0800

    additional deploy testing

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,13 +1,11 @@
+// prettier-ignore
 module.exports = {
   root: true,
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
     quotes: ["error", "double"],
   },
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require("firebase-functions");
+// prettier-ignore
+// const functions = require("firebase-functions");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
